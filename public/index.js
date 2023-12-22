4c8a9d189d99100e1bb988c4132d14990179c1f1
brunogrbavac <bruno.grbavac@outlook.com>
2023-12-22 07:02:18 +0000

    feat: added interview tasks 'solutions'
    Added CSS/HTML/JS code that solves the said interview task.

--- public/index.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('add-box-btn').addEventListener('click', function() {
-    // Candidate will complete this function
-    // Task: Add a new little box into the box-container
-});
---
+++ public/script.js
@@ -0,0 +1,10 @@
+document.getElementById('add-box-btn').addEventListener('click', function() {
+    // Candidate will complete this function
+    // Task: Add a new little box into the box-container
+    
+    // const boxContainer = document.getElementById('box-container');
+    // const template = document.getElementById('box-template').content.cloneNode(true);
+    // const newBox = template.querySelector('.little-box');
+    // newBox.textContent = boxContainer.children.length + 1;
+    // boxContainer.appendChild(newBox);
+});
